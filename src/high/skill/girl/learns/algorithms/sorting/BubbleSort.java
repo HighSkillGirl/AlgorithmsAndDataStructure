e06e760da5da6e8f3c66b3f513bf9709c941667f
vera <HighSkillGirl@yandex.com>
2026-02-16 14:28:54 +0300

    bubble sort

---
+++ src/high/skill/girl/learns/algorithms/sorting/BubbleSort.java
@@ -0,0 +1,36 @@
+package high.skill.girl.learns.algorithms.sorting;
+
+// смысл в многократном прохождении массива и перестановке местами соседних элементов в порядке возрастания
+// сложность по времени O(n^2) - в худшем случае все элементы будут перепутаны и нужно пройти n * n раз
+// сложность по памяти O(1) - при оценке алгритма по памяти учитывается только дополнительная память на расчеты без оценки входных данных
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] data = new int[]{-133, 23, -11, 0, 1999, -11, 7, 23};
+        bubbleSort(data);
+    }
+
+    private static void bubbleSort(int[] array) {
+        boolean while_swap = true;
+
+        while (while_swap) {
+            while_swap = false;
+
+            for (int i = 0; i < array.length; i++) {
+                if (i == array.length - 1) {
+                    break;
+                }
+                int curr = array[i];
+                int next = array[i + 1];
+                if (curr > next) {
+                    array[i] = next;
+                    array[i + 1] = curr;
+                    while_swap = true;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(array));
+    }
+}
