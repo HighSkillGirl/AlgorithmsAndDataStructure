0e0ba456e32ac157251fcf75052028b33e6a5bd7
vera <HighSkillGirl@yandex.com>
2026-02-21 16:20:15 +0300

    add comments

--- src/high/skill/girl/learns/algorithms/sorting/InsertionSort.java
+++ src/high/skill/girl/learns/algorithms/sorting/InsertionSort.java
@@ -1,10 +1,16 @@
 package high.skill.girl.learns.algorithms.sorting;
 
+// суть алгоритма в том, что текущее число изымается из рассмотрения, тем самым мы освобождаем место для сдвигаемых элементов
+// удобна, когда массив почти отсортирован. Используется в потоковой обработке данных и как часть более адаптивных алгоритмов
+// "примитивная, но практичная"
+// сложность по времени - O(n^2)
+// сложность по памяти - O(1)
+
 import java.util.Arrays;
 
 public class InsertionSort {
     public static void main(String[] args) {
-        int[] data = new int[]{5, -1, 8, 6, 4};
+        int[] data = new int[]{4, -1, 8, 6, 5};
         insertionSort(data);
     }
 
--- src/high/skill/girl/learns/algorithms/sorting/SelectionSort.java
+++ src/high/skill/girl/learns/algorithms/sorting/SelectionSort.java
@@ -1,6 +1,7 @@
 package high.skill.girl.learns.algorithms.sorting;
 
 // суть в поиске минимального индекса и перестановке значения 1 раз во внешнем цикле. То есть внутренний цикл нужен для поиска минимума
+// используется в обучении, может применяться для структур с дорогой записью в память (максимум перестановок n - 1)
 // сложность по времени - O(n^2)
 // сложность по памяти - O(1)
 
@@ -8,23 +9,22 @@ import java.util.Arrays;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] data = new int[]{1, 5, 44, -3, 12, -1, 1};
-        int[] data2 = new int[]{-133, 23, -11, 0, 1999, -11, 7, 23};
-        selectionSort(data2);
+        int[] data = new int[]{4, -1, 8, 6, 5};
+        selectionSort(data);
     }
 
     private static void selectionSort(int[] array) {
-        System.out.println(Arrays.toString(array));
         for (int i = 0; i < array.length; i++) {
             int minIndex = i;
             for (int k = i + 1; k < array.length; k++) {
-                if (array[k] < array[minIndex]) {
+                if (array[k] < array[minIndex])
                     minIndex = k;
-                }
             }
-            int temp = array[i];
-            array[i] = array[minIndex];
-            array[minIndex] = temp;
+            if (minIndex != i) {
+                int temp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = temp;
+            }
         }
         System.out.println(Arrays.toString(array));
     }
