94cc725c66cf071f29b35060449e6ac22feba13c
vera <HighSkillGirl@yandex.com>
2026-03-04 10:08:55 +0300

    quick sort add description

--- src/high/skill/girl/learns/algorithms/sorting/QuickSort.java
+++ src/high/skill/girl/learns/algorithms/sorting/QuickSort.java
@@ -1,17 +1,23 @@
 package high.skill.girl.learns.algorithms.sorting;
 
 // сортировка в рекурсии - после деления массива складываем по условию >< чем pivot
-// используется в обучении, так как создание массивов на каждом уровне рекурсии избыточно
+// вариант с массивами используется только в обучении, так как создание массивов на каждом уровне рекурсии избыточно
 // сложность по времени: O(n^2) в худшем случае и O(n log n) в среднем
 // сложность по памяти: O(n^2) в худшем случае и O(n log n) в среднем
+
+// вариант in-place может быть с одним или двумя pivot. Используются разные гибридные варианты в стандартных библиотеках, внутри алгоритмов (QuickSelect)
+// преимущество этого алгоритма сортировки в том, что используется мало памяти (cash-friendly), очень быстрый на случайных данных. Но не стабильный
+// сложность по времени - O(n log n) (глубина рекурсии log n * проход массива на 1 рекурсии n). Если pivot выбран плохо и массив отсортирован - O(n^2)
+// сложность по памяти - O(log n), но в худшем случае - O(n) из-за глубины рекурсии
 
 import java.util.Arrays;
 
 public class QuickSort {
     public static void main(String[] args) {
         int[] data = new int[]{-1, 3, 4, -3, -3, 253, -18, 1};
-        System.out.println(Arrays.toString(arrayQuickSort(data)));
-        System.out.println(Arrays.toString(inPlaceQuickSort(data, 0, data.length - 1)));
+        System.out.println("quick sort through arrays: " + Arrays.toString(arrayQuickSort(data)));
+        inPlaceQuickSort(data, 0, data.length - 1);
+        System.out.println("quick sort in place:       " + Arrays.toString(data));
     }
 
     private static int[] arrayQuickSort(int[] array) {
@@ -52,22 +58,18 @@ public class QuickSort {
         return result;
     }
 
-    private static int[] inPlaceQuickSort(int[] array, int left, int right) {
-        if (left >= right) return array;
+    private static void inPlaceQuickSort(int[] array, int left, int right) {
+        if (left >= right) return;
 
         int pivotValue = array[(left + right) / 2];
-        int i = left;
+        int i = left; // i и j основываются на переданных границах. Внутри цикла происходит их "двигание" туда-сюда
         int j = right;
 
-        while (i <= j) {
-            while (array[i] < pivotValue) {
-                i++;
-            }
-            while (array[j] > pivotValue) {
-                j--;
-            }
+        while (i <= j) { // работаем, пока не пересекутся
+            while (array[i] < pivotValue) i++;
+            while (array[j] > pivotValue) j--;
 
-            if (i <= j) {
+            if (i <= j) { // мы подвигали указатели и проверяем, не пересеклись ли они. Если этого не сделать, значение меньше pivot уйдет в правую часть (а должно в левую)
                 int temp = array[j];
                 array[j] = array[i];
                 array[i] = temp;
@@ -77,9 +79,7 @@ public class QuickSort {
             }
         }
 
-        inPlaceQuickSort(array, left, j);
+        inPlaceQuickSort(array, left, j); // вызываем рекурсию с заново посчитанными диапазонами. Возвращаемое значение не нужно, так как массив один на всё
         inPlaceQuickSort(array, i, right);
-
-        return array;
     }
 }
