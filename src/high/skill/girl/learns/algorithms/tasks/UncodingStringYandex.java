e3a4f76dd3ba05524d6fea65c85d5f2fd306e425
vera <HighSkillGirl@yandex.com>
2026-03-13 17:42:53 +0300

    add second variant, not ready

--- src/high/skill/girl/learns/algorithms/tasks/UncodingStringYandex.java
+++ src/high/skill/girl/learns/algorithms/tasks/UncodingStringYandex.java
@@ -3,9 +3,13 @@ package high.skill.girl.learns.algorithms.tasks;
 public class UncodingStringYandex {
 
     public static void main(String[] args) {
-        String input = "31665"; //31665 //8512#12#15#89
-        StringBuilder result = new StringBuilder(input);
+        String input = "8512#12#15#89"; //31665 //8512#12#15#89
+        //uncodeAlgorithmV1(input);
+        uncodeAlgorithmV2(input);
+    }
 
+    private static void uncodeAlgorithmV1(String input) {
+        StringBuilder result = new StringBuilder(input);
         int hashIndex = input.indexOf('#');
 
         while (hashIndex != -1) {
@@ -24,6 +28,30 @@ public class UncodingStringYandex {
         }
 
         System.out.println(result);
+    }
+
+    private static void uncodeAlgorithmV2(String input) {
+        StringBuilder result = new StringBuilder();
+        int hashIndex = input.indexOf('#');
+        int fromIndex = 0;
+        int toIndex = hashIndex;
+
+        toIndex = input.length();
+
+        String sub1 = input.substring(fromIndex, toIndex);
+        for (int i = 0; i < sub1.length(); i++) {
+            char symbol = sub1.charAt(i);
+            int symbolNumber = symbol - '0';
+            char letter = (char) ('a' + symbolNumber - 1);
+            result.append(letter);
+        }
+            String sub2 = input.substring(hashIndex - 2, hashIndex);
+            result.append(uncodeSubstring(sub2));
+            fromIndex = hashIndex + 1;
+            hashIndex = input.indexOf("#", fromIndex);
+
+        System.out.println(result);
+
     }
 
     private static char uncodeSubstring(String subString) {
